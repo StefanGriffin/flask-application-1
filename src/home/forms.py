7c2ea12afc7f403b92341f12f1f4b121fb3148ae
Stefan Griffin <griffinstef39@gmail.com>
2020-06-06 00:30:43 -0400

    Update 40

--- src/home/forms.py
+++ src/home/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, ValidationError
+from wtforms import StringField, validators, ValidationError,HiddenField
+from sqlalchemy import not_
 from .models import EmailSignup
 
 class LandingForm(FlaskForm):
+    id = HiddenField('id')
     full_name = StringField('Full name',
                             render_kw={"class": "form-control",
                                        "placeholder": "Full-name"},
@@ -21,10 +23,12 @@ class LandingForm(FlaskForm):
                         ])
 
     def validate_email(self, field):
+        _id = self.data.get('id', -1) # -1 is not a valid id
         if field.data.endswith(".edu"):
             raise ValidationError('You cannot use a school email address.')
-        obj = EmailSignup.query.filter_by(email=field.data).first()
+        not_query = not_(EmailSignup.id == _id)  
+        obj = EmailSignup.query.filter_by(email=field.data).filter(not_query).first()
         if obj is not None:
-            msg = 'This email has already been taken'
-            raise ValidationError(msg)
+                msg = 'This email has already been taken'
+                raise ValidationError(msg)
 
--- src/home/views.py
+++ src/home/views.py
@@ -11,9 +11,15 @@ def home():
     if form.validate_on_submit():
         # print(form.full_name.data)
         # print(form.email.data)
-        data = form.data
-        if 'csrf_token' in data:
-            del data['csrf_token']
+        # data = form.data
+        data = {
+            "full_name": form.full_name.data,
+            "email": form.email.data
+        }
+        # if 'csrf_token' in data:
+            # del data['csrf_token']
+        # if 'id' in data:
+            # del data['id']
         obj = EmailSignup.query.filter_by(email=form.email.data).first()
         if obj is None:
             obj = EmailSignup(**data) # (full+name=, email=)
@@ -24,7 +30,6 @@ def home():
     return render_template('home.html', form=form)
 
 '''
-
 create email obj instance via form
 saved in database
 in databases, we have objects with ids in models.py
@@ -33,6 +38,21 @@ primary keys to lookup in database
 use dynamic url routing to lookup object in databses
 
 '''
+@app.route("/items/", methods=['GET'])
+def item_list():
+    object_list = EmailSignup.query.all()
+    return render_template("items/list.html", object_list=object_list)
+
+@app.route("/item/", methods=['GET'])
+def item_list_redirect():
+    return redirect("/items/")
+
+
+@app.route("/items/<int:id>/", methods=['GET'])
+def item_detail_redirect(id):
+    return redirect("/item/{}/".format(id))
+
+
 @app.route('/item/<int:id>/', methods=['GET'])
 def item_detail(id):
     # instance = EmailSignup.query.get(id)
@@ -47,8 +67,14 @@ def item_update(id):
     # instance.email -> form.email
     form = LandingForm(obj=instance)
     if form.validate_on_submit():
-        data = form.data
-        print(data)  
+        full_name = form.full_name.data
+        email = form.email.data
+        instance.full_name = full_name
+        instance.email = email
+        instance.save()
+        return redirect("/item/{}/".format(instance.id))
+        # data = form.data
+        # print(data)  
     return render_template('items/form.html', instance=instance, form=form)
  
 
@@ -56,4 +82,8 @@ def item_update(id):
 def item_delete(id):
     # instance = EmailSignup.query.get(id)
     instance = EmailSignup.query.filter_by(id=id).first_or_404()
-    return render_template('items/detail.html', instance=instance)
+    # form
+    if request.method == "POST":
+        instance.delete()
+        return redirect("/")
+    return render_template('items/delete.html', instance=instance)
