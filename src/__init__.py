99f98d80bc293ca7e0270b4db536a6b06a318c56
Stefan Griffin <griffinstef39@gmail.com>
2020-06-10 22:57:27 -0400

    Update 56 Secret Key changes

--- src/__init__.py
+++ src/__init__.py
@@ -3,14 +3,9 @@ import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate 
-
-# from .home.models import *
-
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
-
 
 # don`t share these in a production environment
 # secret key
@@ -19,23 +14,21 @@ csrf = CSRFProtect(app)
 # Configure the database
 
 app.debug = True 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:patrocle34@localhost/fweb1'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:patrocle34@localhost/fweb1'
 
+# app.secret_key = os.environ.get('SECRET')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 # db_path = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
 # db_uri = 'sqlite:///{}'.format(db_path)
 
 
 app.config.update(dict(
-    SECRET_KEY="c003d035-3181-4fb2-b9ea-91b09f207189",
+    SECRET_KEY="SECRET",
     WTF_CSRF_SECRET_KEY='05cab3d7-5f31-471f-a3b1-366ebb42440c',
 ))
 
